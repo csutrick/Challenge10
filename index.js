9a82cc37e90ee6cd4de26fc8b0d63a3667af5827
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-07-26 23:02:07 -0500

    added user input detection

--- index.js
+++ index.js
@@ -27,12 +27,38 @@ function userQuestions() {
         ])
         .then((answers) => {
             if (answers.text.length > 3) {
-              console.log("Must enter a value of no more than 3 characters");
-              userQuestions();
+                console.log("Must enter a value of no more than 3 characters");
+                userQuestions();
             } else {
-              console.log("Success!")
+                console.log("Making image");
+                makeFile(answers);
             }
           });
+};
+
+function makeFile(answers) {
+    // defaults
+    var svgString = "";
+    var filename = "generatedLogo";
+    // user values
+    let user_text = answers.text;
+    let user_fontColor = answers["text-color"];
+    let user_shapeColor = answers.shape;
+    let user_shapeType = answers["pixel-image"];
+
+    //make the shape
+    if (user_shapeType === "Sqaure") {
+        console.log("Sqaure Selected")
+    } else if (user_shapeType === "Circle") {
+        console.log("Circle Selected")
+    } else if (user_shapeType === "Triangle") {
+        console.log("Triangle Selected")
+    } 
+    // if none are selected error - restarts process
+    else {
+        console.log("Invalid shape, please try again");
+        userQuestions();
+    };
 };
 
 // runs this function first
