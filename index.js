77f5477e03fd07a6cc01592de4d15635a3be48e6
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-07-27 20:25:26 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+// imports
 import fs from 'fs';
 import inquirer from 'inquirer';
+
+
+// shape class
+// makes the shape
+class Shape {
+    constructor() {
+        this.color = '';
+    };
+    // sets color of shape
+    setColor(color) {
+        this.color = color;
+    };
+};
+// SVG circle shape
+class Circle extends Shape {
+    render() {
+        return `<circle cx="50%" cy="50%" r="100" fill="${this.color}" />`;
+    };
+};
+// SVG sqaure shape
+class Square extends Shape {
+    render() {
+        return `<rect x="50" y="50" width="200" height="200" fill="${this.color}" />`;
+    };
+};
+// SVG triangle shape
+class Triangle extends Shape {
+    render() {
+        return `<polygon points="0,200 300,200 150,0" fill="${this.color}" />`;
+    };
+};
+
 
 // makes svg
 class Svg{
@@ -18,7 +51,9 @@ class Svg{
     };
 };
 
+
 var svgString = "";
+
 
 // prompts user questions and stores "answers"
 function userQuestions() {
@@ -59,6 +94,7 @@ function userQuestions() {
             }
           });
 };
+
 
 // gets properties from user prompts and builds shape
 function makeShape(answers) {
@@ -101,46 +137,14 @@ function makeShape(answers) {
 function fileWrite() {
     console.log("writing file");
     var filename = "generatedLogo.svg";
-
     fs.writeFile(filename, svgString, function (err) {
         if (err) {
             return console.log(err);
         }
-        console.log("logo generated!");
+        console.log("Generated logo.svg");
     });
-}
+};
 
 
 // runs this function first
 userQuestions();
-
-
-// shape class
-// makes the shape
-class Shape {
-    constructor() {
-        this.color = '';
-    };
-    // sets color of shape
-    setColor(color) {
-        this.color = color;
-    };
-};
-// SVG circle shape
-class Circle extends Shape {
-    render() {
-        return `<circle cx="50%" cy="50%" r="100" fill="${this.color}" />`;
-    };
-};
-// SVG sqaure shape
-class Square extends Shape {
-    render() {
-        return `<rect x="50" y="50" width="200" height="200" fill="${this.color}" />`;
-    };
-};
-// SVG triangle shape
-class Triangle extends Shape {
-    render() {
-        return `<polygon points="0,200 300,200 150,0" fill="${this.color}" />`;
-    };
-};
